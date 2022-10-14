36b1bdbbfc95905cde09e83f0795a8713b2e86cb
nimuthuRandiya <115784688+nimuthuRandiya@users.noreply.github.com>
2022-10-14 16:46:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94768712510']
+global.premium = ['94768712510']
+global.ownernomer = '94768712510'
+global.ownername = '🐦 nimuthu Randiya'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
